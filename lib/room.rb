b1de0ee1e6fa63ec2d13cc07fd56045998719d3d
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-13 09:19:26 -0600

    Fix default length value, add is_painted & paint methods, and pass related tests; complete iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,22 @@
 class Room
+  attr_reader :category, :length, :width
 
-  def initialize(category, length, width)
+  def initialize(category, length = 10, width)
     @category = category
-    @length = 10
+    @length = length
     @width = width
+    @is_painted = false
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
   end
 end
