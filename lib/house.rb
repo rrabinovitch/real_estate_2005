a472d5ca541433253f79a227f1f34bb925245dda
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-13 10:32:25 -0600

    Add functionality to sort by area

--- lib/house.rb
+++ lib/house.rb
@@ -42,4 +42,16 @@ class House
   def price_per_square_foot
     (@price.to_f / area).round(2)
   end
+
+  def rooms_sorted_by_area
+    rooms_by_area = (@rooms.sort_by { |room| room.area}).reverse
+  end
+
+  # def rooms_sorted_by_category
+  #   rooms_by_area = {}
+  #
+  #   @rooms.map do |room|
+  #     rooms_by_area[room.category] = rooms_from_category(room.category)
+  #   end
+  # end
 end
