3f583fbb12fce9e739af9d99a5e96d3c9c28426d
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-13 10:19:49 -0600

    Switch room.area from method to attribute w attr_reader

--- lib/room.rb
+++ lib/room.rb
@@ -1,15 +1,12 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :area
 
   def initialize(category, length = 10, width)
     @category = category
     @length = length
     @width = width
+    @area = @length * @width.to_i
     @is_painted = false
-  end
-
-  def area
-    @length * @width.to_i
   end
 
   def is_painted?
